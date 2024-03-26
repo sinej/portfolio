7b10e577ba5295f419e23b078b306ad616b6f5d9
shin <sinej1020@gmail.com>
2024-03-26 23:46:41 +0900

    navbar 수정

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+
+import { styles } from "@src/styles";
+import { navLinks } from "@src/con"
+
 
 const Navbar = () => {
     return (
