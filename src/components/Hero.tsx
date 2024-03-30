99d313f9760ba010131be59acc232a817aa9b97a
shin <sinej1020@gmail.com>
2024-03-30 23:47:23 +0900

    다시 배너 수정

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
                     </p>
                 </div>
             </div>
-            <ComputerCanvas/>
+            {/*<ComputerCanvas/>*/}
         </section>
     );
 }
